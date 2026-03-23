8efec2a27568696cedaa6e3bab2a4a0b0a7ee4c0
b5 <sparkle_pony_2000@qri.io>
2026-03-23 01:51:51 -0400

    remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
-pub mod pigeons;
 mod protocol;
 mod service;
 mod ssh;
 mod tunnel;
 
-// pub use pigeons::*;
 pub use service::{Service, ServiceParams, install as install_service, is_installed as service_is_installed, uninstall as uninstall_service};
 pub use ssh::{
     SshConfigPigeonEntry, add_tunnel_host, dot_ssh_secret_key, home_ssh_dir, list_tunnel_hosts,
